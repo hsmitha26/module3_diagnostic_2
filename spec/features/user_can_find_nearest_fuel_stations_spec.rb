57202149d820231dabe1a52ad02499553c8d03bd
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-25 09:22:40 -0600

    adds test to see nearest fuel staiton

--- spec/features/user_can_find_nearest_fuel_stations_spec.rb
+++ spec/features/user_can_find_nearest_fuel_stations_spec.rb
@@ -6,10 +6,11 @@ describe "user can find nearest fuel stations: " do
     click_on 'Find Nearest Station'
     expect(current_path).to eq('/search')
   end
+
+  it "user can see the closes electric fuel station" do
+    visit '/search'
+    
+  end
 end
-# As a user
-# When I visit "/"
-# And I select "Turing" form the start location drop down (Note: Use the existing search form)
-# And I click "Find Nearest Station"
-# Then I should be on page "/search"
+
 # Then I should see the closest electric fuel station to me.

3a993db94b15725e1105a182050d7a8e9d2ac840
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-25 10:36:26 -0600

    successful response from API call to NREL using params['location'] instead of hard coded lat/lng

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,14 @@
 class SearchController < ApplicationController
   def index
-    connection = Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?') do |faraday|
+    response = Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?') do |faraday|
       faraday.params[:api_key] = ENV['nrel_api_key']
-      faraday.params[:latitude] = 39.751312
-      faraday.params[:longitude] = -104.996594
+      faraday.params[:location] = params["location"]
       faraday.params[:radius] = 1.0
       faraday.params[:limit] = 2
     end
     binding.pry
-    @facade = SearchFacade.new(params)
+    all_stations = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+    @facade = SearchFacade.new(all_stations)
   end
 end

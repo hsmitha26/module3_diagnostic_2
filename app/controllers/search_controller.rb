3d58d715752bf1a68eaf1be668059e5ea17fb0c3
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-10-25 10:26:22 -0600

    successfully generates JSON response using hard coded lat/lng

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,13 @@
 class SearchController < ApplicationController
   def index
-    
+    connection = Faraday.get('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?') do |faraday|
+      faraday.params[:api_key] = ENV['nrel_api_key']
+      faraday.params[:latitude] = 39.751312
+      faraday.params[:longitude] = -104.996594
+      faraday.params[:radius] = 1.0
+      faraday.params[:limit] = 2
+    end
+    binding.pry
     @facade = SearchFacade.new(params)
   end
 end
